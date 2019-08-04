eb4cca1fd3285b74e6441453738477ed21c28433
Julian <julianfrancia110@gmail.com>
2019-08-04 00:13:19 -0300

    Agregue jquery bootstrap favicon y popperjs, armado de vista del Login

--- ChatProj-front/ChatProj/src/app/components/login/login.component.ts
+++ ChatProj-front/ChatProj/src/app/components/login/login.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+declare var jQuery: any;
+declare var $: any;
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    var $inputItem = $(".js-inputWrapper");
+    $inputItem.length &&
+      $inputItem.each(function() {
+        var $this = $(this),
+          $input = $this.find(".formRow--input"),
+          placeholderTxt = $input.attr("placeholder"),
+          $placeholder;
+        $input.after('<span class="placeholder">' + placeholderTxt + "</span>"),
+          $input.attr("placeholder", ""),
+          ($placeholder = $this.find(".placeholder")),
+          $input.val().length
+            ? $this.addClass("active")
+            : $this.removeClass("active"),
+          $input
+            .on("focusout", function() {
+              $input.val().length
+                ? $this.addClass("active")
+                : $this.removeClass("active");
+            })
+            .on("focus", function() {
+              $this.addClass("active");
+            });
+      });
   }
 
+  onSubmit() {}
 }
