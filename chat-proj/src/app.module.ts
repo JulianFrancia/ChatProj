62cd320ad6ac75425c928ae6bf0d4953d4a998d2
luther <luciof10.17@gmail.com>
2019-08-10 21:50:13 -0300

    Agrego pipe para validar el body de los post y put. Todavia no es funcional, catchea la excepcion antes que la validacion se haga

--- chat-proj/src/app.module.ts
+++ chat-proj/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { HttpErrorFilter } from './shared/http-error.filter';
 import { LoggingInterceptor } from './shared/logging.interceptor';
+import { ValidationPipe } from './shared/validation.pipe';
 
 @Module({
   imports: [
@@ -24,6 +25,10 @@ import { LoggingInterceptor } from './shared/logging.interceptor';
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
     },
   ],
 })
