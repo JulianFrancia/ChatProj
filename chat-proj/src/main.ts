cd3fe0d8fd0fa89b2edcbec17f3be44f5971ec67
luther <luciof10.17@gmail.com>
2019-08-25 00:25:13 -0300

    Ahora anda el swagger para ver la documentacion de la api

--- chat-proj/src/main.ts
+++ chat-proj/src/main.ts
@@ -4,15 +4,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const hostDomain = AppModule.isDev ? `${AppModule.host}: ${AppModule.port}` : AppModule.host;
+  const hostDomain = AppModule.isDev ? `${AppModule.host}:${AppModule.port}` : AppModule.host;
 
   const swaggerOptions = new DocumentBuilder()
     .setTitle('ChatProject')
     .setDescription('La app que me va a hacer pegarla')
-    .setVersion('1.0')
-    .addTag('chat')
+    .setVersion('1.0.0')
     .setHost(hostDomain.split('//')[1])
     .setSchemes(AppModule.isDev ? 'http' : 'https')
+    .setBasePath('/api')
     .addBearerAuth('Authorization', 'header')
     .build();
 
