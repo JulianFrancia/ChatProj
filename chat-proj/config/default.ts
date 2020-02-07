211e4acaebb477c0e29a23862975d3f204bbbf59
luther <luciof10.17@gmail.com>
2020-02-06 23:43:10 -0300

    Cambio nombre DB a UpperCamelCase

--- chat-proj/config/default.ts
+++ chat-proj/config/default.ts
@@ -1,7 +1,7 @@
 export default {
   HOST: 'http://localhost',
   PORT: 8080,
-  // MONGO_URI: 'mongodb://localhost/chatProj',
-  MONGO_URI: 'mongodb://34.69.232.230:27017,34.69.232.230:27018,34.69.232.230:27019/chatProj?replicaSet=rs0',
+  // MONGO_URI: 'mongodb://localhost/ChatProj',
+  MONGO_URI: 'mongodb://34.69.232.230:27017,34.69.232.230:27018,34.69.232.230:27019/ChatProj?replicaSet=rs0',
   JWT_KEY: 'lfjf',
 };
