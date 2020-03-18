0aa67d9133456b952ca8a6a309fc5c06be0014f6
luther <luciof10.17@gmail.com>
2020-03-18 20:53:30 -0300

    Saco case-sensitive para username y mail

--- chat-proj/src/user/models/user.model.ts
+++ chat-proj/src/user/models/user.model.ts
@@ -3,13 +3,13 @@ import { UserRole } from './user-role.enum';
 import { prop, ReturnModelType, getModelForClass, arrayProp } from '@typegoose/typegoose';
 
 export class User extends BaseModel<User> {
-    @prop({ required: [true, 'username is required'], minlength: [6, 'Must be at least 6 characters'], unique: true })
+    @prop({ lowercase: true, trim: true, required: [true, 'username is required'], minlength: [6, 'Must be at least 6 characters'], unique: true })
     username: string;
 
     @prop({ required: [true, 'password is required'], minlength: [6, 'Must be at least 6 characters'] })
     password: string;
 
-    @prop({ required: [true, 'email is required'], unique: true })
+    @prop({ lowercase: true, trim: true, required: [true, 'email is required'], unique: true })
     email: string;
 
     @prop({ required: [true, 'nick is required'], minlength: [4, 'Must be at least 4 characters'], unique: true })
